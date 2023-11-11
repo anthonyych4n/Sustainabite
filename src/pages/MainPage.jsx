326fa170c27a07828fa21246fbff5b1d4358c0e7
anthonyych4n <anthony168c@gmail.com>
2023-11-11 13:00:47 -0700

    Added files

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,12 +2,13 @@
 import Search from "../components/Search";
 import Footer from "../components/Footer";
 
+
 function MainPage({ setQuery, query }) {
   return (
     <>
       <section>
         <div className="mainpage">
-          <h1 className="mainpageh1">SustainaBites</h1>
+          <h1 className="mainpageh1"></h1>
           <p>A web app to find more sustainable choices to your food.</p>
           <Search setQuery={setQuery} query={query} />
         </div>
